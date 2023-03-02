997762a91c7e98138546f459ad4aca1602697b0e
Juan Martin <juanmartinsesali@gmail.com>
2023-03-02 19:34:03 -0300

    improved animation of pelota

--- public/sketch.js
+++ public/sketch.js
@@ -1,21 +1,18 @@
-
 function setup() {
   let cnv = createCanvas(windowWidth, windowHeight, WEBGL)
-  cnv.position(0,0)
+  cnv.position(0, 0)
   cnv.style('pointer-events', 'none')
 }
 
 function draw() {
   clear()
   translate(width * 0.35, -130)
-  rotateY(mouseY/2)
-  rotateX(mouseX/2)
-
+  rotateX(((-mouseY) * 50 + millis()) / 5000)
+  rotateY(((-mouseX) * 50 + millis() * 2) / 7000)
   noFill()
-  stroke(255,150)
+  stroke(255, 150)
   sphere(100, 24)
 }
-
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
